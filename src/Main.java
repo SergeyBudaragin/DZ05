df0a6798736944243b6494316e9811073f1276c7
SergeyBudaragin <lestate@rambler.ru>
2019-03-17 14:50:21 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,6 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
@@ -21,62 +18,69 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
+        try {
+            System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
 
-        driver = new ChromeDriver();
-        String url = "https://www.rgs.ru/";
-        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.get(url);
+            driver = new ChromeDriver();
+            String url = "https://www.rgs.ru/";
+            driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+            driver.get(url);
 
-        findByXpathAndClick("//ol/li/a[contains(text(),'Страхование')]");
-        findByXpathAndClick("//*[contains(text(),'Выезжающим')]");
+            findByXpathAndClick("//ol/li/a[contains(text(),'Страхование')]");
+            findByXpathAndClick("//*[contains(text(),'Выезжающим')]");
 
-        scrollByXpath("//a[contains(text(),'Рассчитать')]");
-        findByXpathAndClick("//a[contains(text(),'Рассчитать')]");
+            scrollByXpath("//a[contains(text(),'Рассчитать')]");
+            findByXpathAndClick("//a[contains(text(),'Рассчитать')]");
 
-        Thread.sleep(2000);
-        textCompare("//body/div/div/div/span[contains(@class, 'h1')]", "Страхование выезжающих за рубеж");
+            Thread.sleep(2000);
+            textCompare("//*[contains(@class,'page-header')]/span", "Страхование выезжающих за рубеж");
 
-        findByXpathAndClick("//*[contains(text(), 'Несколько')]");
-        fillFormCountries("Countries", "Испания");
+            scrollByXpath("//*[contains(text(), 'Несколько')]");
+            findByXpathAndClick("//*[contains(text(), 'Несколько')]");
+            fillFormCountries("Countries", "Испания");
 
-        scrollByXpath("//form/div/div/div/div/input[contains(@class, 'form-control width-xs-9rem ')]");
-        fillFormXpath("//form/div/div/div/div/input[contains(@class, 'form-control width-xs-9rem ')]", dateGenerate());
+            scrollByXpath("//*[contains(@data-bind,'FirstD')]");
+            fillFormXpath("//*[contains(@data-bind,'FirstD')]", dateGenerate());
 
-        findByXpathAndClick("//*[contains(text(), 'Не более 90')]");
+            scrollByXpath("//*[contains(text(), 'Не более 90')]");
+            findByXpathAndClick("//*[contains(text(), 'Не более 90')]");
 
-        fillFormXpathName("//input[contains(@class,'form-control')][@data-test-name='FullName']", "IVANOV", "IVAN");
+            scrollByXpath("//input[contains(@class,'form-control')][@data-test-name='FullName']");
+            fillFormXpathName("//input[contains(@class,'form-control')][@data-test-name='FullName']", "IVANOV", "IVAN");
 
-        scrollByXpath("//*[@data-test-name='BirthDate']");
-        fillFormXpath("//*[@data-test-name='BirthDate']", "11121998");
+            scrollByXpath("//*[@data-test-name='BirthDate']");
+            fillFormXpath("//*[@data-test-name='BirthDate']", "11121998");
 
-        scrollByXpath("//div[10]/div[1]/div[1]/div[1]/div/input");
-        clickCheckBox("//div[10]/div[1]/div[1]/div[1]/div");
+            scrollByXpath("//div[contains(@data-bind,'active')]/div[contains(@class,'toggle')]");
+            clickCheckBox("//div[contains(@data-bind,'active')]/div[contains(@class,'toggle')]");
 
-        scrollByXpath("//input[contains(@data-test-name , 'IsProcessingPersonalDataTo')]");
-        clickCheckBox("//input[contains(@data-test-name , 'IsProcessingPersonalDataTo')]");
+            scrollByXpath("//input[contains(@data-test-name , 'IsProcessingPersonalDataTo')]");
+            clickCheckBox("//input[contains(@data-test-name , 'IsProcessingPersonalDataTo')]");
 
-        scrollByXpath("//*[@data-test-name='NextButton'][contains(@data-bind,'Misc.NextButton')]");
-        findByXpathAndClick("//*[@data-test-name='NextButton'][contains(@data-bind,'Misc.NextButton')]");
+            scrollByXpath("//*[@data-test-name='NextButton'][contains(@data-bind,'Misc.NextButton')]");
+            findByXpathAndClick("//*[@data-test-name='NextButton'][contains(@data-bind,'Misc.NextButton')]");
 
-        scrollToTop();
-        Wait<WebDriver> wait = new WebDriverWait(driver, 10, 2000);
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//*[contains(text(),'Расчет')]"))));
-        scrollByXpath("//*[contains(text(),'Расчет')]");
-        findByXpathAndClick("//*[contains(text(),'Расчет')]");
+            Thread.sleep(5000);
 
-        textCompare("//div[1]/div[3]/div/div/div/div[1]/span[2]/span", "Многократные поездки в течение года");
-        textCompare("//span/span/strong[contains(@data-bind, 'text: Name')]", "Шенген");
-        textCompare("//strong[contains(@data-bind, 'text: Last')]", "IVANOV IVAN");
-        textCompare("//strong[contains(@data-bind, 'text: Birth')]", "11.12.1998");
-        textCompare("//div[7]/div[1]/div/span[2]/span", "Включен");
+            scrollByXpath("//*[contains(@class,'summary-value')][contains(@data-bind,'Trips')]");
+            textCompare("//*[contains(@class,'summary-value')][contains(@data-bind,'Trips')]", "Многократные поездки в течение года");
+            textCompare("//span/span/strong[contains(@data-bind, 'text: Name')]", "Шенген");
+            textCompare("//strong[contains(@data-bind, 'text: Last')]", "IVANOV IVAN");
+            textCompare("//strong[contains(@data-bind, 'text: Birth')]", "11.12.1998");
+            textCompare("//div[contains(@data-bind, 'Актив')]/div[@class='summary-row']/span[@class='summary-value']/span", "Включен");
 
-        Thread.sleep(2000);
+            Thread.sleep(1000);
 
 
-        driver.close();
+            driver.close();
+        } catch (Exception e) {
+            driver.close();
+            System.out.println("Ошибка " + e);
+
+        }
     }
+
 
     /**
      * Метод нажимающий чек-бокс если он не активен
@@ -120,15 +124,6 @@ public class Main {
         WebElement ScrollLocation = driver.findElement(By.xpath(xPath));
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].scrollIntoView(true);", ScrollLocation);
-    }
-
-
-    /**
-     * Метод скроллящий наверх страницы
-     */
-    public static void scrollToTop() {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollTo(0, -document.body.scrollHeight)");
     }
 
     /**
